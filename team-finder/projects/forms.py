b1f08b0f597911c70e9be1bb3364d4dfd109aeca
Nick Baryshev <nicksgame05@gmail.com>
2026-05-31 00:59:12 +0700

    Update forms.py

--- team-finder/projects/forms.py
+++ team-finder/projects/forms.py
@@ -3,17 +3,7 @@ from urllib.parse import urlparse
 from django import forms
 
 from .models import Project
-
-
-def validate_github(url):
-    if not url:
-        return url
-    host = (urlparse(url).netloc or "").lower()
-    if host.startswith("www."):
-        host = host[4:]
-    if host != "github.com":
-        raise forms.ValidationError("Ссылка должна вести на github.com")
-    return url
+from ..users.utils import validate_github
 
 
 class ProjectForm(forms.ModelForm):
